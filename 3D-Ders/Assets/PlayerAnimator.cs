4cb464a3b0994dd928bb3429d244e618bf87881e
EnesKaya2 <enes.kaya7541@gmail.com>
2023-09-07 21:07:21 +0300

    07.09.2023

---
+++ 3D-Ders/Assets/PlayerAnimator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private const string IS_WALKING = "IsWalking";
+
+    [SerializeField] Player player;
+    [SerializeField] Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool(IS_WALKING,player.IsWalking());
+    }
+}
